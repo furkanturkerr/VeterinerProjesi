1ee4600d62fb7a275def21fae997eee2597c1855
furkanturker <furkanturker.dev@gmail.com>
2025-12-26 19:03:05 +0300

    Hakkımızda alanı yeni temayla uyumlu hale getirildi

--- WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee7b09422f992323a12e253a453dfdb6bc77953f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4784291356563dbf767a20fac8bd13298cdc9a64")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
